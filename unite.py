242f7f388f1a0fcfa7a8e69a36f98134a702669e
David Milum <david.milum.89@gmail.com>
2015-01-24 17:20:45 -0500

    cli parsing adequate. moving on to playing with implementations

--- unite.py
+++ unite.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python2
 import globals
 import getopt, sys, os
+import os.path
+
+def add_key():
+    pass
+
+def track():
+    pass
+
+def init():
+    pass
 
 def usage():
     print("Usage: \n\
@@ -10,13 +20,29 @@ def usage():
     unite track --diff <file> \n\
     unite add-key <key> \n")
 
+def standard_args_check(opts):
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+
+def file_is_trackable(f):
+    pass
+
+
 def main():
+    private_keys_dir = os.getenv("UNITE_PRIVATE_KEYS",
+            os.getenv("HOME",None) + "/" + ".ssh")
     unite_root = os.getenv("UNITE_ROOT",
             os.getenv("HOME",None) + "/" + ".unite")
     try:
         action = sys.argv[1]
-        opts, args = getopt.getopt(sys.argv[2:], "hk:r:d", ["help", "key=", "relpath=", "diff" ])
-        last = sys.argv[-1]
+        last = ""
+        if action == "track" or action == "add-key": # expecting to use last element for this
+            opts, args = getopt.getopt(sys.argv[2:-1], "hk:r:d", ["help", "key=", "relpath=", "diff" ])
+            last = sys.argv[-1]
+        else:
+            opts, args = getopt.getopt(sys.argv[2:], "hk:r:d", ["help", "key=", "relpath=", "diff" ])
         print(args)
         print(opts)
         print(action)
@@ -26,19 +52,28 @@ def main():
         print(str(err)) # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
-    output = None
-    verbose = False
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-o", "--output"):
-            output = a
+    if action == "init":
+        standard_args_check(opts)
+    if action == "track":
+        standard_args_check(opts)
+        fname = last
+        print(fname)
+        if os.path.isfile(fname):
+            for o, a in opts:
+                if o in ("-k", "--key"):
+                    pass
+                elif o in ("-r", "--relpath"):
+                    pass
+                elif o in ("-d", "--diff"):
+                    pass
+                else:
+                    assert False, "unhandled option"
         else:
-            assert False, "unhandled option"
-    # ...
+            assert False, "File " + fname + " does not exist."
+    if action == "add-key":
+        standard_args_check(o, a)
+        key = last
+
 
 if __name__ == "__main__":
     main()
